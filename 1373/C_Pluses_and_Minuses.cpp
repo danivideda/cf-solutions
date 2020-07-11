1e05a799436de6f676ead20893270386eaf97e4d
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-07-12 06:54:09 +0700

    wip#1373_C

--- 1373/C_Pluses_and_Minuses.cpp
+++ 1373/C_Pluses_and_Minuses.cpp
@@ -12,7 +12,35 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
   // freopen("input.txt", "r", stdin);
-  int a, b, c;
+  int t;
+  cin >> t;
+  string s;
+
+  for (int j = 0; j < t; j++) {
+    cin >> s;
+    bool ok;
+    int res = 0, cur;
+    ll init = 1;
+
+    while (init) {
+      cur = init - 1;
+      init++;
+      ok = true;
+
+      for (int i = 0; i < s.length(); i++ ) {
+        res++;
+        s[i] == '+' ? cur++ : cur--;
+        if (cur < 0) {
+          ok = false;
+          break;
+        }
+      }
+
+      if (ok) break;
+    }
+
+    cout << res << endl;
+  }
 
   return 0;
 }
